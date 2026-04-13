92af3f3667f82c3a134db54f1981e741cf303779
kaushal <kaushal.choudhury2007@gmail.com>
2026-04-14 01:03:00 +0530

    Optimize Vercel build script

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-# Install Flutter from the stable channel
+# Clone Flutter with depth 1 to save massive download time and Vercel disk space
 if [ ! -d "flutter" ]; then
-  git clone https://github.com/flutter/flutter.git -b stable
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1
 fi
 
 # Add Flutter to the PATH
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# Enable web support (just in case)
-flutter config --enable-web
+# Disable analytics to prevent CI prompts from hanging the build
+flutter config --no-analytics
+
+# Pre-download binaries specifically for web
+flutter precache --web
 
 # Get dependencies and build
 flutter pub get
